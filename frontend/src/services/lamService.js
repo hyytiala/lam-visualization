69bb1f3dcdcb840b69a14cc173f988782e3429b4
Otto Hyytiälä <otto.hyytiala@gmail.com>
2020-05-06 19:10:25 +0300

    get url from env

--- frontend/src/services/lamService.js
+++ frontend/src/services/lamService.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
+const URL = process.env.PROD === 'true' ? 'https://spotishare.live/lam' : 'http://localhost:3001'
+console.log(URL)
+
 const getStations = async () => {
   const response = await axios.get('https://tie.digitraffic.fi/api/v3/metadata/tms-stations')
   return response.data
@@ -11,7 +14,7 @@ const getVolume = async () => {
 }
 
 const getStationData = async (year, ely, lam, day) => {
-  const response = await axios.get(`http://localhost:3001/api?year=${year}&ely=${ely}&lam=${lam}&day=${day}`)
+  const response = await axios.get(`${URL}/api?year=${year}&ely=${ely}&lam=${lam}&day=${day}`)
   return response.data
 }
 
