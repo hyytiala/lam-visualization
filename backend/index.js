aeec68c662cc60dedced711cd238f003f2116e41
hyytiala <otto.hyytiala@gmail.com>
2021-12-06 16:23:37 +0200

    added eslint

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,29 @@
-  
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const cors = require('cors')
-const dotenv = require('dotenv')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
-if (process.env.NODE_ENV !== 'production') {
-    dotenv.config()
-    app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+  app.use(
+    morgan(":method :url :status :res[content-length] - :response-time ms")
+  );
 }
 
-const lamDataRouter = require('./routes/lamData')
+const lamDataRouter = require("./routes/lamData");
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use('/lam/api', lamDataRouter)
+app.use("/lam/api", lamDataRouter);
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.use((req, res, next, err) => {
-    console.log(err)
-})
+  console.log(err);
+});
--- backend/routes/lamData.js
+++ backend/routes/lamData.js
@@ -1,28 +1,34 @@
-const express = require('express')
-const { spawn } = require('child_process')
+const express = require("express");
+const { spawn } = require("child_process");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/check', (req, res) => {
-  return res.send('Backend running')
-})
+router.get("/check", (req, res) => {
+  return res.send("Backend running");
+});
 
-router.get('/', (req, res) => {
-  let dataToSend
-  const python = spawn('python3', ['./scripts/parser.py', req.query.year, req.query.ely, req.query.lam, req.query.day])
-  python.stdout.on('data', function (data) {
-    console.log('Pipe data from python script ...')
-    dataToSend = data.toString()
-  })
+router.get("/", (req, res) => {
+  let dataToSend;
+  const python = spawn("python3", [
+    "./scripts/parser.py",
+    req.query.year,
+    req.query.ely,
+    req.query.lam,
+    req.query.day,
+  ]);
+  python.stdout.on("data", function (data) {
+    console.log("Pipe data from python script ...");
+    dataToSend = data.toString();
+  });
 
-  python.on('close', (code) => {
-    console.log(`child process close all stdio with code ${code}`)
+  python.on("close", (code) => {
+    console.log(`child process close all stdio with code ${code}`);
     if (code === 0) {
-      res.send(dataToSend)
+      res.send(dataToSend);
     } else {
-      res.sendStatus(404)
+      res.sendStatus(404);
     }
-  })
-})
+  });
+});
 
-module.exports = router
+module.exports = router;
